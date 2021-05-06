46411d97028b884b09ddc4c179414a363d548983
Felipe Augusto <51971892+Felipe-Augusto-Correia@users.noreply.github.com>
2021-05-06 00:47:44 -0300

    design feito

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: BallPage(),
       ),
     );
@@ -9,11 +10,9 @@ void main() => runApp(
 class BallPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Ball(
-      
-    );
+    return Ball();
   }
-} 
+}
 
 class Ball extends StatefulWidget {
   @override
@@ -23,8 +22,17 @@ class Ball extends StatefulWidget {
 class _BallState extends State<Ball> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        backgroundColor: Colors.grey[500],
+        title: Text('Ask Me Anything'),
+        centerTitle: true,
+      ),
+      // ignore: deprecated_member_use
+      body: Center(
+        child: Image.asset('images/ball1.png'),
+      ),
     );
   }
 }
